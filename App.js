5a619b95e57997c2a1944243796055ec136aa7ed
Ketan Suthar <ketan.suthar@saama.com>
2024-01-27 12:41:44 +0530

    step3 added

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {
-        id: "p1"
-    }, 
-    React.createElement("div", 
-        { id: "p1"}, 
-        [
-            React.createElement("h1", {}, "Hello there"),
-            React.createElement("h2", {}, "Hello there")
-        ]
-    )
-);
-
 
 const heading = React.createElement("h1", {
     id: "heading"
@@ -20,6 +8,43 @@ const heading = React.createElement("h1", {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+console.log(heading);
 
-console.log(parent);
+// JSX - not HTML, its like HTML syntax
+// JSX code is transpiled before by (Babel)parcel
+// Babel will convert below line to ReactDOM.createElement
+const jsxHeading = (
+    <h1 id="heading">Hello from JSX</h1>
+);
+
+root.render(jsxHeading);
+
+console.log(jsxHeading);
+
+
+
+/*
+React Component
+- Class based - old 
+- Functional based - new 
+    - just normal js function
+*/
+
+// Functional Component - it returns JSX
+const HeadingComponent = () => {
+    return <h1 id="heading">Hello from Functional Component</h1>;
+}
+
+const text = "Hello from ComponentComposition"
+
+const ComponentComposition = () => (
+    <div>
+        {HeadingComponent()}
+        <HeadingComponent />
+        <HeadingComponent></HeadingComponent>
+        <h2>Text: {text}</h2>
+    </div>
+)
+
+root.render(<ComponentComposition />);
+
