92eb269eb0305a632dc21841862b5aeb2f94add6
Ketan Suthar <ketan.suthar@saama.com>
2024-02-01 22:59:16 +0530

    1. home cards ui changed

--- src/components/Body.js
+++ src/components/Body.js
@@ -27,8 +27,6 @@ const Body = () => {
     }
 
     const onlineStatus = useOnlineStatus();
-    const VegCard = withVegLabel(RestaurantCard)
-    const NonVegCard = withNonVegLabel(RestaurantCard)
 
     if(onlineStatus === false) {
         return (
@@ -42,39 +40,12 @@ const Body = () => {
 
     return (
         <div className="body">
-            <div className="filter flex">
-                <div className="m-4 p-4">
-                    <input type="text" className="border border-solid border-black"
-                        value={searchText} onChange={(e)=>{
-                        setSearchText(e.target.value)
-                    }}/>
-                    <button className="px-4 py-2 bg-gray-100 m-4 rounded-lg" onClick={()=>{
-                        const result = resDataState.filter((i) => i.info.name.toLowerCase().includes(searchText.toLowerCase()));
-                        if(result && result.length > 0)
-                            setFilteredResDataState(result);
-                    }}>Search</button>
-                </div>
-                <div className="m-4 p-4 flex items-center">
-                    <button className="filter-btn px-4 py-2 bg-gray-100 m-4 rounded-lg" onClick={() => {
-                        setFilteredResDataState(resDataState.filter((i) => i.info.avgRatingString >= 4.4));
-                    }}>Top Restaurants
-                    </button>
-                </div>
-                <div className="m-4 p-4  flex items-center">
-                    <label>UserName <input type="text" className="border border-black p-2"
-                                        value={loggedInUser} 
-                                        onChange={(e) => setUserName(e.target.value)}/></label>
-                </div>
-            </div>
-
-            <div className="restaurant-container flex flex-wrap">
+            <div className="restaurant-container flex min-h-screen w-full flex-wrap content-center justify-between p-5 bg-gray-100">
                 {
                     filteredResDataState?.map(
                         i => (
                            <Link key={i.info.id} to={"/restaurant/" + i.info.id}> 
-                                {
-                                    i?.info?.veg ? (<VegCard data={i} />) : (<NonVegCard data={i} />)
-                                }
+                                <RestaurantCard data={i} />
                            </Link>
                         )
                     )
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,41 +3,23 @@ import { CDN_URL } from "../utils/constants"
 const RestaurantCard = (props) => {
     const { data } = props
     const { info } = data
-    const couisine = data.info.cuisines.join(", ")
+
     return (
-        <div className="m-4 p-4 w-52 restaurant-card bg-gray-100 hover:bg-yellow-100">
-            <img className="restaurant-logo"
-            alt="restaurant-logo"
-            src={CDN_URL + info.cloudinaryImageId}></img>
-            <h3>{info.name}</h3>
-            <h4>{couisine}</h4>
-            <h4>{info.avgRatingString} stars</h4>
-            <h4>{info.slaString}</h4>
+        <div className="w-60 bg-white p-3 m-7">
+            <img alt="image" className="h-40 w-full object-cover" src={CDN_URL + info.cloudinaryImageId} />
+            <ul className="mt-3 flex flex-wrap">
+                <li className="mr-auto">
+                        {info.name}
+                </li>
+                <li className="mr-2">
+                        {info.avgRatingString}
+                </li>
+                <li>
+                        {info.slaString}
+                </li>
+            </ul>
         </div>
     )
-}
-
-
-export const withVegLabel = (RestaurantCard) => {
-    return (props) => {
-        return (
-            <div>
-                <label className="absolute bg-green-500 m-2 p-2"></label>
-                <RestaurantCard {...props} />
-            </div>
-        )
-    }
-}
-
-export const withNonVegLabel = (RestaurantCard) => {
-    return (props) => {
-        return (
-            <div>
-                <label className="absolute bg-red-500 m-2 p-2"></label>
-                <RestaurantCard {...props} />
-            </div>
-        )
-    }
 }
 
 export default RestaurantCard;
